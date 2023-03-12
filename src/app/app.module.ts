e792bb13f035b23a570e018579f7a138f7a05f32
Vineetha Namburi (vinnambu) <namburivineetha9@gmail.com>
2023-03-12 12:12:10 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
+import { PaymentComponent } from './payment/payment.component';
+import { TicketComponent } from './ticket/ticket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutusComponent,
+    ContactusComponent,
+    DetailsComponent,
+    HomeComponent,
+    MoviesComponent,
+    PaymentComponent,
+    TicketComponent
   ],
   imports: [
     BrowserModule,
